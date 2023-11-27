078ff7c19b148d1b4dad71987bdec91f44307e1e
agoodbook <152195027+agoodbook@users.noreply.github.com>
2023-11-28 00:00:35 +0800

    Initial commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'agoodbook', // Usually your GitHub org/user name.
+  projectName: 'agoodbook.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
