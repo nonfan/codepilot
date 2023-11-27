612624ab88ca617b36d56fcba99f864af4c9c2f0
BeHer0 <beher0@foxmail.com>
2023-11-28 00:39:50 +0800

    Initial commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'agoodbook', // Usually your GitHub org/user name.
   projectName: 'agoodbook.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
