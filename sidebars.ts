90ae2bd42bac4c063138c9825ecd6373cdeeb3c5
agoodbook <beher0@foxmail.com>
2023-12-20 01:29:54 +0800

    add: 封装入口按钮组件（图标、标题）

--- sidebars.ts
+++ sidebars.ts
@@ -1,14 +1,14 @@
 module.exports = {
-    html5Sidebar: [
-        {
-            "type":"doc",
-            "id": "started",
-            "label": "介绍"
-        },
-        {
-            "type":"doc",
-            "id": "guide",
-            "label": "指南"
-        }
-    ]
+  html5Sidebar: [
+    {
+      type: "doc",
+      id: "started",
+      label: "介绍",
+    },
+    {
+      type: "doc",
+      id: "guide",
+      label: "指南",
+    },
+  ],
 };
