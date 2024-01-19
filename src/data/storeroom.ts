15792c2a6deb792a4510609f2e8ef9b653fe9e1a
agoodbook <beher0@foxmail.com>
2024-01-20 00:00:45 +0800

    add: 添加更多导航项

--- src/data/storeroom.ts
+++ src/data/storeroom.ts
@@ -1,6 +1,7 @@
 import { Navigations } from "@site/src";
 import NextraSvg from "@site/src/assets/icons/NextraSvg";
 import StorybookSvg from "@site/src/assets/icons/StorybookSvg";
+import ReactSvg from "@site/src/assets/icons/ReactSvg";
 
 /**
  * 静态站点生成器
@@ -25,5 +26,19 @@ export const FrontendTools: Navigations = [
       "Storybook 是一个用于开发和测试 React、Vue、Angular 等前端组件的开源工具。它提供了一个独立的开发环境，让开发者能够以组件为单位进行开发、测试和文档编写，同时保持组件与整个应用的解耦。",
     url: "https://agoodbook.github.io/storybook/",
     icon: StorybookSvg,
+  },
+];
+
+/**
+ * 其它类别
+ */
+
+export const Other: Navigations = [
+  {
+    title: "ComponentLibrary",
+    smallTitle:
+      "ComponentLibrary 是自己搭建的一个React组件库，方便使用，并发布了相应的npm包(component-library)",
+    url: "https://agoodbook.github.io/component-library/",
+    icon: ReactSvg,
   },
 ];
