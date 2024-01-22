43b987b7224b2abc40fab92e8bc435bc7657c27c
agoodbook <beher0@foxmail.com>
2024-01-23 01:44:15 +0800

    feat: 对readme文件美化

--- src/data/up-projects.ts
+++ src/data/up-projects.ts
@@ -8,7 +8,7 @@ export const UIComponents: Navigations = [
   {
     title: "AGoodBook-UI",
     smallTitle:
-      "AGoodBook-UI 是自己搭建的一个组件库，方便使用与维护，并发布了相应的npm包(component-library)，并实现Vue、React两种组件库",
+      "AGoodBook-UI 是自己搭建的一个组件库，方便使用与维护，并发布了相应的npm包(agoodbook-ui)，并实现Vue、React两种组件库",
     url: "https://agoodbook.github.io/agoodbook-ui/",
     icon: AGoodBookUISvg,
   },
