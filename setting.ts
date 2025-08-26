4bc584480a87445d002b0184547c60cb8eb804f6
nonfan <bc.mofan@icloud.com>
2025-08-26 15:40:50 +0800

    fix

--- setting.ts
+++ setting.ts
@@ -3,7 +3,7 @@ export const repository: string = "codepilot";
 export const title: string = "CodePilot";
 export const logoSrc: string = "./images/logo.svg";
 export const logoSrcDark: string = "./images/logoDark.svg";
-export const favicon: string = "./images/logoDark.svg";
+export const favicon: string = "./images/logo.svg";
 
 export const algolia = {
   appId: "xx",
